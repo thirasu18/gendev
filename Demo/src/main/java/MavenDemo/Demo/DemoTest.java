341a5ac2ce1f8bc25f396904f6159e50694e6bcc
thirasu18 <thilgurra@gmail.com>
2019-06-27 00:47:33 -0500

    Initialize git pub

---
+++ Demo/src/main/java/MavenDemo/Demo/DemoTest.java
@@ -0,0 +1,10 @@
+package MavenDemo.Demo;
+
+public class DemoTest {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("welcome back");
+	}
+
+}
---
+++ Demo/src/test/java/MavenDemo/Demo/TngTest.java
@@ -0,0 +1,13 @@
+package MavenDemo.Demo;
+
+import org.testng.annotations.Test;
+
+public class TngTest {
+    @Test
+	public  void loginTest() {
+		
+		System.out.println("Login is Successful");
+
+	}
+
+}
